7111218630f25c91d96ecd2764a87f6ca86a2266
Erik Slovák <erik.slovak7@gmail.com>
2024-10-31 20:47:42 +0100

    feat: add initial reproduction

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,7 @@
+export default function Dashboard() {
+  return (
+    <div className="flex flex-col gap-8 items-center justify-center h-screen w-screen">
+      <h1 className="text-4xl font-bold">Dashboard</h1>
+    </div>
+  );
+}
